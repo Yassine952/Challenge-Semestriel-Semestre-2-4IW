be900e3ad39048e76e063b120199482bfadd0e38
Yassine952 <yassine95210@hotmail.com>
2024-07-26 18:21:19 +0000

    fix front

--- client/src/views/ResetPassword.vue
+++ client/src/views/ResetPassword.vue
@@ -16,7 +16,7 @@
               type="password"
               v-model="newPassword"
               required
-              class="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
+              class="w-full px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
             />
           </div>
           <button
